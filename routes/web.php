d53d22823d3bd3de420a8ef4cd8a1e11b1de02d8
Asep Salas <kangsep350@gmail.com>
2024-03-20 13:48:50 +0700

    Merubah File web untuk routing

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,16 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('my-name', function() {
+    echo "Asep Salas";
+});
+
+Route::get('my-city/{param}', function($city) {
+    echo "Kota saya di  " . $city;
+});
+
+Route::get('get-student/{name}/{code}', function($name,$code) {
+    echo "Nama saya " . $name. " NRP " . $code;
+});
+
